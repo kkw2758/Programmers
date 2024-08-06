5f31cf9778f893c0f57a0d865a7bae9b9f8ddd26
kkw2758 <41050653+kkw2758@users.noreply.github.com>
2024-08-06 01:00:27 +0000

    점 찍기 문제풀이

---
+++ Algorithm/구현/LEVEL2/점 찍기.py
@@ -0,0 +1,12 @@
+import math
+
+def solution(k, d):
+  answer = 0
+
+  for x in range(0, d + 1, k):
+    y = int(math.sqrt(d ** 2 - x ** 2))
+    answer += y // k + 1
+  return answer
+
+print(solution(2, 4))
+print(solution(1, 5))
